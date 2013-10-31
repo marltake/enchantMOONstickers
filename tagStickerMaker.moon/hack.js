dea926a97509ac7677fb7f0600db51ce2b39ba2e
marltake <otake.shigenori@gmail.com>
2013-11-01 01:20:59 +0900

    array is index filled list

--- tagStickerMaker.moon/hack.js
+++ tagStickerMaker.moon/hack.js
@@ -8,10 +8,19 @@ importJS(["lib/MOON.js", "lib/enchant.js", "lib/ui.enchant.js", "lib/color.encha
             stickertap: function(event) {
                 var p = MOON.getCurrentPage();
                 var pj = MOON.getPaperJSON(p["backing"]);
-		var c=[[768,1024][0,0]];
-		//var buf="";
-		//for(st in pj['strokes']){ buf+=(" "+st['data'].length); }
-                MOON.alert(pj['strokes'][0]['data'].slice(0,6), MOON.finish);
+		var c={x:768,y:1024,X:0,Y:0};
+		for(var i=pj['strokes'].length-1; i>=0; i--){
+			var dt=pj['strokes'][i]['data'];
+			for(var j=dt.length-3;j>=0;j-=3){
+				var x=dt[j];
+				var y=dt[j+1];
+				if(c['x']>x){c['x']=x;}
+				if(c['y']>y){c['y']=y;}
+				if(c['X']<x){c['X']=x;}
+				if(c['Y']<y){c['Y']=y;}
+			}
+		}
+                MOON.alert(c, MOON.finish);
                 enchant.puppet.stopTheatre();
             },
             stickerattach: function(event) {
