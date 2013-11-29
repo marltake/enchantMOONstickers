d48d0e2086e24b2fc241963e62023f4503de0de7
marltake <otake.shigenori@gmail.com>
2013-11-29 16:51:00 +0900

    Query修正

--- dateTimeSealTag.moon/hack.js
+++ dateTimeSealTag.moon/hack.js
@@ -146,7 +146,7 @@ importJS(["lib/MOON.js", "lib/enchant.js"], function() {
         MOON.finish();
     };
     sticker.ontap = function() {
-        MOON.penPrompt("search pattern","^2003",{fieldLength:16,fieldSize:56,inputType:"any"}, function(str){str="function matches(j){for(k in j){if(k.matches(/"+str+"/) && j[k]>0){return true;}}return false;}"; MOON.searchStorage(str);MOON.finish();});
+        MOON.penPrompt("search pattern","^2013",{fieldLength:16,fieldSize:56,inputType:"any"}, function(str){str="function matches(j){for(var k in j){if(k.matches(/"+str+"/) && j[k]>0){return true;}}return false;}"; MOON.searchStorage(str);MOON.finish();});
         MOON.finish();
     };
     sticker.register();
